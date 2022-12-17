d71ead60d7f07287d23e24958e13ec764cbc9021
vaishnavi borkar <vaishnaviborkar29@gmail.com>
2022-12-17 23:35:19 +0530

    day-5

--- helpinghand/src/App.js
+++ helpinghand/src/App.js
@@ -1,11 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-import Footer from './Components/Footer';
+// import Footer from './Components/Footer';
 import AllRoutes from './Routes/AllRoutes';
-import Navbar from "./Components/Navbar";
-import Home from './Routes/Home';
-import Register from "./Routes/Register";
+// import Navbar from "./Components/Navbar";
+// import Home from './Routes/Home';
+// import Register from "./Routes/Register";
 // import Register from './Routes/Register';
+// import SliderBox from "./Data/SliderBox"
 function App() {
   return (
     <div className="App">
@@ -14,7 +15,7 @@ function App() {
       {/* <Home /> */}
       {/* <Footer /> */}
       {/* <Register /> */}
-
+      {/* <SliderBox /> */}
     </div>
   );
 }
